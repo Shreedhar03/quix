b8c52c68574f92bfe3bf5a4dcc83a264e58af4b8
Shreedhar <urawane03@gmail.com>
2024-01-31 13:17:46 +0530

    update creation screen

--- creator/src/components/Question.tsx
+++ creator/src/components/Question.tsx
@@ -10,7 +10,7 @@ import { Checkbox } from './ui/checkbox'
 import { CheckedState } from '@radix-ui/react-checkbox'
 
 
-const Question = () => {
+const Question = ({index}:{index:number}) => {
     const [noOfOptions, setNoOfOptions] = React.useState(1)
     const [includesCorrectAnswer, setIncludesCorrectAnswer] = React.useState(false)
     const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -28,11 +28,11 @@ const Question = () => {
     }
     return (
         <>
-            <div className='bg-cyan p-3 rounded-lg z-40'>
+            <div className={`${index%2==0 ? 'bg-peach/25' : 'bg-cyan/25'} p-3 rounded-xl border border-foreground z-40`}>
                 <input type="text"
                     autoFocus
                     placeholder='Add a question'
-                    className='focus:outline-none bg-inherit font-semibold border-b-2 focus:border-b-blue border-b-blue/50 text-xl p-2 placeholder:text-blue/70 w-10/12' />
+                    className='focus:outline-none bg-transparent font-semibold border-b-2 focus:border-b-blue border-b-blue/50 text-xl p-2 placeholder:text-blue/70 w-10/12' />
                 <div className='my-4'>
                     {/* <h3 className='text-lg  font-semibold ml-1'>Options</h3> */}
                     <div className="flex items-center space-x-2 ml-1 my-3">
--- creator/src/pages/Creation.tsx
+++ creator/src/pages/Creation.tsx
@@ -1,3 +1,4 @@
+import CreationNav from '@/components/CreationNav'
 import Logo from '@/components/Logo'
 import Question from '@/components/Question'
 import { Button } from '@/components/ui/button'
@@ -9,60 +10,63 @@ const Creation = () => {
     const options = ['Randomize question order', 'Reveal correct answer', 'Show Leaderboard after each question']
     const [noOfQuestions, setNoOfQuestions] = React.useState(1)
     return (
-        <section className='min-h-screen bg-blue p-4 sm:p-16'>
-            <Logo variant='monochrome' />
-            <div className='flex justify-between gap-4 flex-col md:flex-row'>
-                <form className='mt-12 w-full md:w-1/2'>
-                    <input type="text"
-                        autoFocus
-                        placeholder='Enter quiz title'
-                        className='focus:outline-none bg-blue text-cyan font-semibold border-b-2 focus:border-b-cyan border-b-cyan/50 text-3xl p-2 placeholder:text-cyan/70 w-11/12' />
-                    {/* ==========Questions========== */}
-                    <div className='mt-6 flex flex-col gap-3 z-40'>
-                        {
-                            Array.from({ length: noOfQuestions }).map((_, i) => (
-                                <Question key={i} />
-                            ))
-                        }
-                    </div>
-                    <Button type='button' className='mt-4 w-full'
-                        onClick={
-                            () => setNoOfQuestions(noOfQuestions + 1)
-                        }>Add Question +</Button>
-                </form>
-
-                <aside className='flex flex-col gap-5 sticky top-0'>
-                    <div className='bg-cyan p-4 rounded-lg'>
-                        <div className='flex items-center gap-2 mt-2'>
-                            <Switch className='w-10 h-6 data-[state=checked]:bg-black data-[state=unchecked]:bg-black/50' />
-                            <p>
-                                Presentation Mode
-                            </p>
-                            <InfoIcon className='text-slate-700' />
+        <>  
+            <CreationNav />
+            <section className='min-h-screen p-4 sm:px-16 sm:pb-16 pt-0 relative bottom-8'>
+                <Logo variant='coloured' />
+                <div className='flex justify-between gap-4 flex-col md:flex-row'>
+                    <form className='mt-12 w-full md:w-1/2'>
+                        <input type="text"
+                            autoFocus
+                            placeholder='Enter quiz title'
+                            className='focus:outline-none text-blue dm-serif font-medium border-b-2 focus:border-b-blue border-b-blue/50 text-3xl p-2 placeholder:text-blue/70 w-11/12' />
+                        {/* ==========Questions========== */}
+                        <div className='mt-6 flex flex-col gap-3 z-40'>
+                            {
+                                Array.from({ length: noOfQuestions }).map((_, i) => (
+                                    <Question key={i} index={i}/>
+                                ))
+                            }
                         </div>
-                        <Button className='mt-4 w-full'>Go Live</Button>
-                    </div>
-                    {/* More Options */}
-                    <div className='bg-cyan p-4 rounded-lg'>
-                        {
-                            options.map((option, i) => (
-                                <div className='flex items-center justify-between gap-2 mt-2' key={i}>
-                                    <div className='flex items-center gap-2'>
-                                        <Switch className='w-10 h-6 data-[state=checked]:bg-black data-[state=unchecked]:bg-black/50' />
-                                        <p className='w-36 text-sm font-medium'>
-                                            {option}
-                                        </p>
+                        <Button type='button' variant={'outline'} className='mt-4 w-full'
+                            onClick={
+                                () => setNoOfQuestions(noOfQuestions + 1)
+                            }>Add Question +</Button>
+                    </form>
+
+                    <aside className='flex flex-col gap-5 sm:sticky sm:top-12'>
+                        <div className='bg-peach/25 p-4 rounded-xl border border-foreground'>
+                            <div className='flex items-center gap-2 mt-2'>
+                                <Switch className='w-10 h-6 data-[state=checked]:bg-black data-[state=unchecked]:bg-black/50' />
+                                <p>
+                                    Presentation Mode
+                                </p>
+                                <InfoIcon className='text-slate-700' />
+                            </div>
+                            <Button className='mt-4 w-full'>Go Live</Button>
+                        </div>
+                        {/* More Options */}
+                        <div className='bg-peach/25 p-4 rounded-xl border border-foreground'>
+                            {
+                                options.map((option, i) => (
+                                    <div className='flex items-center justify-between gap-2 mt-2' key={i}>
+                                        <div className='flex items-center gap-2'>
+                                            <Switch className='w-10 h-6 data-[state=checked]:bg-black data-[state=unchecked]:bg-black/50' />
+                                            <p className='w-36 text-sm font-medium'>
+                                                {option}
+                                            </p>
+                                        </div>
+                                        <InfoIcon className='text-slate-700' />
                                     </div>
-                                    <InfoIcon className='text-slate-700' />
-                                </div>
-                            ))
-                        }
-                    </div>
-                </aside>
-            </div>
+                                ))
+                            }
+                        </div>
+                    </aside>
+                </div>
 
 
-        </section>
+            </section>
+        </>
     )
 }
 
